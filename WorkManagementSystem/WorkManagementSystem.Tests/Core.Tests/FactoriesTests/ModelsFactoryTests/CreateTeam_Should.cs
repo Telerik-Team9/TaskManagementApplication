dd981af4229e364e198f6f06c37d59360db2f21f
Ali Marekov <a.marekov@outlook.com>
2020-08-15 17:47:23 +0300

    Add ModelsFactory CreateTeam unit tests

---
+++ WorkManagementSystem/WorkManagementSystem.Tests/Core.Tests/FactoriesTests/ModelsFactoryTests/CreateTeam_Should.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WorkManagementSystem.Core.Contracts;
+using WorkManagementSystem.Core.Factories;
+using WorkManagementSystem.Models;
+using WorkManagementSystem.Models.Contracts;
+
+namespace WorkManagementSystem.Tests.Core.Tests.FactoriesTests.ModelsFactoryTests
+{
+    [TestClass]
+    public class CreateTeam_Should
+    {
+        [TestMethod]
+        public void CreateTeamWhen_ValidValuesArePassed()
+        {
+            //Arrange
+            string name = new string('a', 7);
+            IModelsFactory factory = new ModelsFactory();
+
+            //Act
+            ITeam team = new Team(name);
+
+            //Assert
+            Assert.AreEqual(name, team.Name);
+            Assert.IsInstanceOfType(team, typeof(ITeam));
+        }
+    }
+}
