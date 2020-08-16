02c5f4970cba4194bb217a16e47e28a46329c342
Ali Marekov <a.marekov@outlook.com>
2020-08-16 11:54:26 +0300

    Add Common/ListAllWorkitems(type feedback) unit tests

---
+++ WorkManagementSystem/WorkManagementSystem.Tests/Core.Tests/Common/ListMethodsTests/ListAllFeedbacks_Should.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkManagementSystem.Core.Common;
+using WorkManagementSystem.Core.Contracts;
+using WorkManagementSystem.Core.Factories;
+using WorkManagementSystem.Models.Contracts;
+using WorkManagementSystem.Tests.Fakes;
+
+namespace WorkManagementSystem.Tests.Core.Tests.Common.ListMethodsTests
+{
+    [TestClass]
+    public class ListAllFeedbacks_Should
+    {
+
+        [TestMethod]
+        public void CorrectlyListAllFeedbacks()
+        {
+            //Arrange
+            IInstanceFactory factory = new InstanceFactory();
+            string expected = "Type: Feedback | Id: 4 | Title: SeedDataFeedback";
+            Func<IWorkItem, string> criteria = x => "Type: " + x.GetWorkItemType() + " | Id: " + x.Id + " | Title: " + x.Title;
+
+            //Act
+            string actual = ListMethods.ListAllWorkItems(factory, criteria, "Feedback");
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void ThrowWhen_NoFeedbacksInDatabase()
+        {
+            //Arrange
+            IInstanceFactory factory = new FakeInstanceFactory();
+            Func<IWorkItem, string> criteria = x => "Type: " + x.GetWorkItemType() + " | Id: " + x.Id + " | Title: " + x.Title;
+
+            //Act and assert
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                string actual = ListMethods.ListAllWorkItems(factory, criteria, "Feedback");
+            });
+        }
+    }
+}
