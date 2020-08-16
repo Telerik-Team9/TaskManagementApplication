05b2b898454379920b01a9e8abbbd01ae29b1d1a
Ali Marekov <a.marekov@outlook.com>
2020-08-16 12:40:30 +0300

    Add Database/ListAllWorkitems method unit tests

---
+++ WorkManagementSystem/WorkManagementSystem.Tests/Core.Tests/DatabaseTests/ListAllWorkItems_Should.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using WorkManagementSystem.Core;
+using WorkManagementSystem.Core.Contracts;
+using WorkManagementSystem.Models.Contracts;
+
+namespace WorkManagementSystem.Tests.Core.Tests.DatabaseTests
+{
+    [TestClass]
+    public class ListAllWorkItems_Should
+    {
+        [TestMethod]
+        public void ReturnsNewListOfWorkitems()
+        {
+            //Arrnage
+            IDatabase db = new Database();
+
+            //Act
+            var result = db.ListAllWorkitems();
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void ReturnListOfWorkitems()
+        {
+            //Arrnage
+            IDatabase db = new Database();
+
+            //Act
+            var result = db.ListAllWorkitems();
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(List<IWorkItem>));
+        }
+    }
+}
