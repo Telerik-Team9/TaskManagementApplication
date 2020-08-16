3d05a50ed7ecb7d3f3eea0a56c9aba27e0330e5c
Ali Marekov <a.marekov@outlook.com>
2020-08-16 09:52:48 +0300

    Add Common/ListAllUnits unit tests

---
+++ WorkManagementSystem/WorkManagementSystem.Tests/Core.Tests/Common/ListMethodsTests/ListAllUnits_Should.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WorkManagementSystem.Core.Common;
+using WorkManagementSystem.Core.Contracts;
+using WorkManagementSystem.Core.Factories;
+
+namespace WorkManagementSystem.Tests.Core.Tests.Common.ListMethodsTests
+{
+    [TestClass]
+    public class ListAllUnits_Should
+    {
+        [TestMethod]
+        public void ListCorrectlyAllMembers()
+        {
+            //Arrange
+            IInstanceFactory factory = new InstanceFactory();
+            string expected = "Maggie\r\nAliii";
+
+            //Act
+            string actual = ListMethods.ListAllUnits(factory, x => x.Name, "member");
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ListCorrectlyAllBoards()
+        {
+            //Arrange
+            IInstanceFactory factory = new InstanceFactory();
+            string expected = "FIRSTBRD\r\nSECONDBRD";
+
+            //Act
+            string actual = ListMethods.ListAllUnits(factory, x => x.Name, "board");
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ThrowWhen_NoMembersIsDatabase()
+        {
+            //Arrange
+            InstanceFactory factory = new InstanceFactory();
+            factory.Database.Members.Clear();
+
+            //Act and assert
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                string actual = ListMethods.ListAllUnits(factory, x => x.Name,"member");
+            });
+        }
+
+        [TestMethod]
+        public void ThrowWhen_NoBoardsIsDatabase()
+        {
+            //Arrange
+            InstanceFactory factory = new InstanceFactory();
+            factory.Database.Boards.Clear();
+
+            //Act and assert
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                string actual = ListMethods.ListAllUnits(factory, x => x.Name, "board");
+            });
+        }
+    }
+}
