fb7a75ec73b773b49087ec32f56887f018bf62cb
Maggie <magi.nikolova@gmail.com>
2020-08-18 00:23:22 +0300

    Modify ListBugsCommand

--- WorkManagementSystem/WorkManagementSystem.Core/Commands/ListCommands/ListBugsCommand.cs
+++ WorkManagementSystem/WorkManagementSystem.Core/Commands/ListCommands/ListBugsCommand.cs
@@ -23,7 +23,7 @@ namespace WorkManagementSystem.Core.Commands.ListCommands
             // Filter
             if (parameters[0].ToLower() == "status")
             {
-                var status = Enum.Parse<BugStatus>(parameters[1] ,true);
+                var status = Enum.Parse<BugStatus>(parameters[1], true);
                 filteredCollection = filteredCollection
                     .Where(b => b.Status == status)
                     .ToList();
@@ -31,17 +31,21 @@ namespace WorkManagementSystem.Core.Commands.ListCommands
 
             else if (parameters[0].ToLower() == "assignee")
             {
+                if (!this.InstanceFactory.Database.Members.Any(m => m.Name == parameters[1]))
+                    throw new ArgumentException("No member with that name.");
+
                 filteredCollection = filteredCollection
                     .Where(b => b.Assignee.Name == parameters[1])
                     .ToList();
             }
 
             // Sort
-            if (!string.IsNullOrEmpty(parameters[2].ToLower()))
+            if (!string.IsNullOrEmpty(parameters[2]))
             {
-                filteredCollection = filteredCollection
+                /*filteredCollection = filteredCollection
                     .OrderBy(GetSortFilter(parameters[2], parameters[3]))
-                    .ToList();
+                    .ToList();*/
+                filteredCollection = GetSortFilter(parameters[2], filteredCollection.ToList());
             }
 
             return string.Join(NewLine, filteredCollection.Select(x => x.PrintInfo()));
@@ -63,7 +67,6 @@ namespace WorkManagementSystem.Core.Commands.ListCommands
             string[] propertyFilter = this.Reader.Read().Split('-');
             parameters.Add(propertyFilter[0]);
 
-
             if (propertyFilter.Length == 1)
             {
                 parameters.Add("");
@@ -72,36 +75,25 @@ namespace WorkManagementSystem.Core.Commands.ListCommands
             else if (propertyFilter.Length == 2)
             {
                 parameters.Add(propertyFilter[1]);
-
             }
-
 
             // Get sorting filter
             this.Writer.WriteLine("Do you want to sort by property? (title/priority/severity)" + NewLine
-                + "If yes - enter property type then property value separated by '-'."
                 + "If not leave this empty");
-            string[] sortFilter = this.Reader.Read().Split('-');
-
-            if (sortFilter.Length == 1)
-            {
-                parameters.Add("");
-            }
-
-            else if (sortFilter.Length == 2)
-            {
-                parameters.Add(sortFilter[1]);
-            }
+            string sortFilter = this.Reader.Read();
+            parameters.Add(sortFilter);
 
             return parameters;
         }
 
-        private Func<IBug, bool> GetSortFilter(string property, string value)
+        private IList<IBug> GetSortFilter(string property, List<IBug> filteredCollection)
         {
             return property switch
             {
-                "title" => w => w.Title == value,
-                "priority" => w => w.Priority == Enum.Parse<Priority>(value),
-                "severity" => w => w.Severity == Enum.Parse<BugSeverity>(value),
+                // remove everything after ==
+                "title" => filteredCollection.OrderBy(w => w.Title).ToList(),
+                "priority" => filteredCollection.OrderBy(w => w.Priority).ToList(),
+                "severity" => filteredCollection.OrderBy(w => w.Severity).ToList(),
 
                 _ => null
             };
